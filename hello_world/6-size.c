a23e448d098cd693afddb494a9a5dcd468397e0e
Ergi004 <ergilama98@gmail.com>
2023-06-16 05:13:56 -0700

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of long int: %zu byte(s)\n", sizeof(lintType));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(llintType));
-	printf("Size of float: zu byte(s)\n", sizeof(floatType));
+	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
 
 	return (0);
 }

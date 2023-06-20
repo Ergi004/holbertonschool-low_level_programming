076937ffaaa1252a8c77ee3689226eecd2bd4fd3
Ergi004 <ergilama98@gmail.com>
2023-06-20 07:22:56 -0700

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,14 +8,7 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int main(void)
-{
-	/**
-	* _putchar - writes the character c to stdout
-	* @c: The character to print
-	*
-	* Return: On success 1.
- 	* On error, -1 is returne
- 	* */
+{/**asdasdsd*/
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');

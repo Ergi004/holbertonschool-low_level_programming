ead420fd8903106ce93dc438aa418cc098a2a9e5
Ergi004 <ergilama98@gmail.com>
2023-06-20 07:19:55 -0700

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,6 +9,13 @@
  */
 int main(void)
 {
+	/**
+	* _putchar - writes the character c to stdout
+	* @c: The character to print
+	*
+	* Return: On success 1.
+ 	* On error, -1 is returne
+ 	* */
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');

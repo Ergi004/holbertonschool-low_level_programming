9a5f32aacf4d2377b952ce7ae964d211484d43ee
Ergi004 <ergilama98@gmail.com>
2023-06-20 07:16:34 -0700

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,17 +8,17 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 
- int main(void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-	_putchar('t'); 
-	_putchar('c'); 
+	_putchar('t');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
+
 	return (0);
 }

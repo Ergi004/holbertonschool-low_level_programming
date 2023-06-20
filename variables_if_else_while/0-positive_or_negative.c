344b94bac6fd1adf5d28e9f8e3aa375eece525dd
Ergi004 <ergilama98@gmail.com>
2023-06-20 01:59:43 -0700

    0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int n = 7777;
+
+int main(void)
+{
+	if ( n > 0)
+	       printf("Number is positive\n");
+	else if ( n == 0 )
+  		printf("Number is 0\n");	
+	else
+       		printf("Number is negative");
+	return (0);
+}	

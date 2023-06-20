ce74fc8d8777ad829d002c2df9d3697f5d706060
Ergi004 <ergilama98@gmail.com>
2023-06-20 03:55:23 -0700

    3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -13,10 +13,10 @@ int main(void)
 {
 	char low;
 
-	if (low = 'a'; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
 
-	for (low = 'A'; low <= 'Z'; low++)
+	if (low = 'A'; low <= 'Z'; low++)
 		putchar(low);
 		putchar('\n');
 

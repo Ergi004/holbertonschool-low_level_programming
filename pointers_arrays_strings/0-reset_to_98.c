3c80e1cb3722c8192045dd954aeebe184bb36991
Ergi004 <ergilama98@gmail.com>
2023-06-26 01:59:50 -0700

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates a varible by pointer
+ * @n: Variable to be updated
+ * Return: adfasdfa
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

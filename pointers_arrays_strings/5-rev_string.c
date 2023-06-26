27bd198c34208ec7577830c0c7bbeac8c2660d44
Ergi004 <ergilama98@gmail.com>
2023-06-26 05:49:23 -0700

    function that prints every other character of a string, starting with the first character, followed by a new line.

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_rev - function that prints a string,in reverse,followed by a new line
+ * @s: string
+ * Return: adfasdfa
+ */
+void rev_string(char *s)
+{
+	int start;
+	int end;
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++
+	}
+
+	start = 0;
+	end = len - 1;
+	
+	while (start < end)
+	{
+		tmp = s[start];
+		s[start] = s[end];
+		s[end] = tmp;
+		start++;
+		end--;
+	}
+}

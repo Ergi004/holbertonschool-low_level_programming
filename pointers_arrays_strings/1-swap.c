058fda0411a205126ddd4e9a9de777476385a005
Ergi004 <ergilama98@gmail.com>
2023-06-26 02:08:43 -0700

    1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - Swaps the values of 2 integers
+ * @a: Variable to be swaped
+ * @b: Variable to be swaped
+ * Return: adfasdfa
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 98, *b = 42;
+	*a = *b, *b = 98;
+}

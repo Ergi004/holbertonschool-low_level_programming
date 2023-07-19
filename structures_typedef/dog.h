c99b5de8c79b41c09159513c92c4f08e77f3ded4
Ergi004 <ergilama98@gmail.com>
2023-07-19 12:59:33 -0700

    0. Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - user-defined structure in C that represents a dog
+ *@name: input
+ *@age: input
+ *@owner: input
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif

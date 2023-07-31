e680e0cd97fc1a64157ac51b5dce159a043de9f4
Ergi004 <ergilama98@gmail.com>
2023-07-31 05:39:27 -0700

    task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -25,11 +25,11 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node->str == NULL)
 	{
 		free(new_node->str);
+		free(new_node);
 		return (NULL);
 	}
-	new_node->str = strdup(str);
-	new_node->len = strlen(str);
-
+	strcpy(new_node->str, str);
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 

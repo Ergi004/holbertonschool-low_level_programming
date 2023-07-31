0f49e6c8ca109842073acd7bb083b95b9c4f909d
Ergi004 <ergilama98@gmail.com>
2023-07-31 03:27:34 -0700

    task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - singly linked list
+ * @count - size_t
+ * @h: string
+ * Return: count
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+		count++;
+		h = h->next;
+	}
+	return (count);
+}

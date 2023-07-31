7049705edfd2d0295663241e072c603c41e2385b
Ergi004 <ergilama98@gmail.com>
2023-07-31 04:47:10 -0700

    task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 	size_t len = strlen(str);
 
 	new_node = malloc(sizeof(list_t));
-	new_node->str = malloc(sizeof(char) *len);
+	new_node->str = malloc(sizeof(char) * len);
 	if (new_node == NULL || str == NULL)
 	{
 		free(new_node->str);

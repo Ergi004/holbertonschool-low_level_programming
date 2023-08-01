e25b1469fee1f3d52000623df5fc302282e193d3
Ergi004 <ergilama98@gmail.com>
2023-08-01 03:17:32 -0700

    Ky eshte tasku zero

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -11,13 +11,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i = 0;
-
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-		i++;
+		printf("%i\n", h->n);
+		count++;
 		h = h->next;
 	}
-	return (i);
-}	
+	return (count);
+}

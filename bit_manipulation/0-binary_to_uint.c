571e67c91f3a8cfb1eabbf7442b603e1cd37f72f
Ergi004 <ergilama98@gmail.com>
2023-08-03 10:30:44 -0700

    Bit Manipulation

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -7,18 +7,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int c;
-	int i;
+	unsigned int n = 0;
+	int i = 0;
 
+	if (b == NULL)
+		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] < 0 && b[i] > 1)
+		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
-
+		n = n << 1;
+		if (b[i] == '1')
+		{
+			n += 1;
+		}
 	}
-	if (b == NULL)
-	{
-		return (0);
-	}
+	return (n);
+}

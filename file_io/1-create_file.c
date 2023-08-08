dae78f63c5565bb2db7ab0a3e3d5b3faa3569836
Ergi004 <ergilama98@gmail.com>
2023-08-08 02:43:58 -0700

    function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,7 +8,7 @@
 /**
  * create_file - Creates a file
  * @filename: Input
- * @Text_content: input
+ * @text_content: input
  * Return: file_r
  */
 int create_file(const char *filename, char *text_content)
@@ -35,4 +35,4 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	return (1);
-}	
+}

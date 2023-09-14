6534d45ce7a66da3ed5b990f65b93ab150224593
Ergi004 <ergilama98@gmail.com>
2023-09-14 04:28:45 -0700

    Searches the value in an array if integers using linear searching

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,6 +1,12 @@
 #include "search_algos.h"
 
 /**
+ * linear_search - Searches for a value in an integerarray using linear search.
+ * @array: Pointer to the integer array to search in.
+ * @size: The number of elements in the array.
+ * @value: The value to search for in the array.
+ * Return: The index of the first occurrence of @value in @array,
+ * or -1 if @value is not found.
  */
 
 int linear_search(int *array, size_t size, int value)
@@ -13,7 +19,7 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if(array[i] == value)
+		if (array[i] == value)
 			return (i);
 	}
 	return (-1);

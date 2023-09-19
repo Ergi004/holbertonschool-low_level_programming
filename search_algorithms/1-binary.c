b61961597aca0886dd123a93be60055114adaec4
Ergi004 <ergilama98@gmail.com>
2023-09-19 02:36:37 -0700

    binary search

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -20,7 +20,7 @@ int binary_search(int *array, size_t size, int value)
 	{
 		mid = (first + last) / 2;
 		printf("Searching in array: ");
-		for (i = first; i < last; i++)
+		for (i = first; i <= last; i++)
 		{
 			if (i == first)
 				printf("%d", array[i]);
